982dc51ab6232ecfad6c2b044177d5d2598bb96a
BruceZCQ <brucezcq@gmail.com>
2018-06-22 14:09:49 +0800

    remove invalid comment

--- src/main/java/cn/zhucongqi/oauth2/controllers/OAuth2Controller.java
+++ src/main/java/cn/zhucongqi/oauth2/controllers/OAuth2Controller.java
@@ -22,58 +22,26 @@ public class OAuth2Controller extends ControllerExt {
 	//using ControllerExt auto init the service instance
 	private OAuthService oauthService;
 
-	/**
-	 * generate code 
-	 * <br/>
-	 * using OAuthRequest <br/>
-	 * IGAuthorizationRequestValidator <br/>
-	 */
 	@ActionKey(ActionUrls.AUTHORIZE_URL)
 	public void onAuthorize() {
 		this.oauthService.authrize();
 	}
 
-	/**
-	 * validate code
-	 * <br/>
-	 * using OAuthGrantRequest <br/>
-	 * AccessTokenRequestValidator <br/>
-	 */
 	@ActionKey(ActionUrls.AUTHORIZE_CODE_URL)
 	public void onAuthorizeCode() {
 		this.oauthService.authrizeCode();
 	}
 	
-	/**
-	 * get secure access token
-	 * <br/>
-	 * using OAuthGrantRequest <br/>
-	 * PasswordCredentialValidator <br/>
-	 * ClientCredentialValidator <br/>
-	 */
 	@ActionKey(ActionUrls.SECURE_ACCESS_TOKEN_URL)
 	public void onAccessTokenSecure() {
 		this.oauthService.secureAccessToken();
 	}
-	
 
-	/**
-	 * get access token
-	 * <br/>
-	 * using OAuthRequest <br/>
-	 * ImplicitValidator <br/>
-	 */
 	@ActionKey(ActionUrls.ACCESS_TOKEN_URL)
 	public void onAcessToken() {
 		this.oauthService.accessToken();
 	}
 	
-	/**
-	 * refresh access token , return new access token and new refresh token
-	 * <br/>
-	 * using OAuthGrantRequest <br/>
-	 * RefreshTokenValidator <br/>
-	 */
 	@ActionKey(ActionUrls.REFRESH_TOKEN_URL)
 	public void onRefreshToken() {	
 		this.oauthService.refreshToken();
